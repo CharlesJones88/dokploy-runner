51b27743a4faf1e9d3569ec79bc1f4973ba89875
Charles <ultramixerman@gmail.com>
2025-08-17 14:42:20 -0600

    logging

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ ACCESS_TOKEN=$TOKEN
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
+echo "REPO ${REPO}"
+echo "REG TOKEN ${REG_TOKEN}"
+
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN}
