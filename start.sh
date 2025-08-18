95aea3b2598e622887e15f7d6ba3c704559092fc
Charles <ultramixerman@gmail.com>
2025-08-17 20:55:29 -0600

    Update start up script to add extra labels

--- start.sh
+++ start.sh
@@ -3,11 +3,28 @@
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 
+ARCH=""
+
+case "$(uname -m)" in
+  "x86_64")
+    ARCH="x64"
+    ;;
+  "aarch64")
+    ARCH="arm64"
+    ;;
+  *)
+    echo "Unsupport OS Architecture"
+    exit 1
+    ;;
+esac
+
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${REPO} \
+  --token ${REG_TOKEN} \
+  --labels dokploy,${ARCH}
 
 cleanup() {
   echo "Removing runner ..."
