e1c1416c8e9ce69e2799c7f0f6c3b090071ddb14
Charles <ultramixerman@gmail.com>
2025-11-22 09:47:23 -0700

    Fix _jq

--- prepull.sh
+++ prepull.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-source /home/charles/.bashrc
 
-for NODE in $(echo "$NODES" | jq -r '.[] | @base64'); do
+for node in $(echo "$NODES" | jq -r '.[] | @base64'); do
   _jq() {
-    echo "${row}" | base64 --decode | jq -r ${1}
+    echo "${node}" | base64 --decode | jq -r ${1}
   }
-	echo "--- Running on $NODE ==="
+	echo "--- Running on $node ---"
 	(
-		ssh "$(_jq '.username')"@"$(_jq '.host')" "GHCR_USERNAME=$GH_USERNAME GHCR_TOKEN=$GHCR_TOKEN bash -s" < ./prepull-latest.sh
+		ssh "$(_jq '.username')@$(_jq '.host')" "GHCR_USERNAME=$GH_USERNAME GHCR_TOKEN=$GHCR_TOKEN bash -s" < ./prepull-latest.sh
 	) &
 done
 
