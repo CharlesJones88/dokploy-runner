00e6a5fdcbea701f83f77881fb9c1e1ba02f8dd7
Charles <ultramixerman@gmail.com>
2025-11-22 14:39:31 -0700

    export vars

--- prepull.sh
+++ prepull.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-export GH_USERNAME GHCR_TOKEN IMAGE
-
 for node in $(echo "$NODES" | jq -r '.[] | @base64'); do
   _jq() {
     echo "${node}" | base64 --decode | jq -r ${1}
   }
 	echo "--- Running on $node ---"
 	(
-		ssh -o StrictHostKeyChecking=no "$(_jq '.username')@$(_jq '.host')" "GHCR_USERNAME=$GH_USERNAME GHCR_TOKEN=$GHCR_TOKEN IMAGE=$IMAGE bash -s" < ./prepull-latest.sh
+		ssh -o StrictHostKeyChecking=no "$(_jq '.username')@$(_jq '.host')" "export GHCR_USERNAME='$GH_USERNAME' GHCR_TOKEN='$GHCR_TOKEN' IMAGE='$IMAGE'; bash -s" < ./prepull-latest.sh
 	) &
 done
 
