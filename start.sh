47963bbc3ccb6c58030af5370b5af46d3c8e6f1f
Charles <ultramixerman@gmail.com>
2026-01-15 22:39:32 -0700

    Fix variable

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ if [[ "$EPHEMERAL" = "true" ]]; then
   ADDITIONAL_LABELS="$ADDITIONAL_LABELS,ephemeral"
 fi
 
-[[ -n "$ADDITION_LABELS" && "$ADDITIONAL_LABELS" != ,* ]] && ADDITION_LABELS=",${ADDITION_LABELS}"
+[[ -n "$ADDITIONAL_LABELS" && "$ADDITIONAL_LABELS" != ,* ]] && ADDITIONAL_LABELS=",${ADDITIONAL_LABELS}"
 
 case "$(uname -m)" in
   "x86_64")
